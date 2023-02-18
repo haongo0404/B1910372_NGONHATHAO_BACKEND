f6bcc1e385c82adea627f65b3c909c7afccf9fdf
Haongo <Haob1910372@student.ctu.edu.vn>
2023-02-18 08:53:46 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const { json } = require("express/lib/response");
 const app = express();
+const contactsRouter=require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
 app.get("/",(req,res)=>{
     res.json({message:"Wellcom to contact book application."});
 });
+app.use("/api/contacts", contactsRouter);
 module.exports=app;
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+exports.create =(req,res) =>{
+    res.send({message:" create handler"});
+};
+exports.findALL=(req,res) =>{
+    res.send({message:"FindALL handler"});
+};
+exports.findOne=(req,res) =>{
+    res.send({message:"FindOne handler"});
+};
+exports.update=(req,res) =>{
+    res.send({message:"update handler"});
+};
+exports.delete=(req,res) =>{
+    res.send({message:"delete handler"});
+};
+exports.deleteALL=(req,res) =>{
+    res.send({message:"deleteALL handler"});
+};
+exports.findALLFavorite=(req,res) =>{
+    res.send({message:"FindALLFavorite handler"});
+};
